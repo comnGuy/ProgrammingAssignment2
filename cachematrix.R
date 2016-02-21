4c6e50d86872059f3cdff3ca474d7125700cfe94
comnGuy <bpblub@gmail.com>
2016-02-21 23:43:59 +0100

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Example
 
-## Write a short comment describing this function
+# source("cachematrix.R")
+# my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+# my_matrix$getMatrix()
+#      [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
 
+# cacheSolve(my_matrix)
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+
+
+
+# Caches the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
 
+	# Contains the inverse matrix
+ 	inverseMatrix <- NULL
+
+	# Sets the matrix
+      setMatrix <- function(y) {
+      	x <<- y
+            inverseMatrix <<- NULL
+      }
+
+	# Returns the matrix
+      getMatrix <- function() {
+		x
+	}
+
+	# Sets the inverve matrix
+      setInverseMatrix <- function(inverse) {
+		inverseMatrix <<- inverse
+	}
+
+	# Returns the inverse matrix
+      getInverseMatrix <- function() {
+		inverseMatrix
+	}
+ 
+	# Returns the functions
+      list(setMatrix = setMatrix,
+           getMatrix = getMatrix,
+           setInverseMatrix = setInverseMatrix,
+           getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
-
+# Computes the inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# Tries to get the matrix
+	inverseMatrix <- x$getInverseMatrix()
+
+	# If the matrix exists, bring it back
+	if (!is.null(inverseMatrix)) {
+		return(inverseMatrix)
+	}
+
+	# Get the matrix
+	matrix <- x$getMatrix()
+
+	# Inverse the matrix
+	inverseMatrix <- solve(matrix, ...)
+
+	# Store the matrix
+	x$setInverseMatrix(inverseMatrix)
+
+	# Return
+	inverseMatrix 
 }
